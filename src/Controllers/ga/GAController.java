5e2d1912c9881c5d35c4fe77ddfa1aa766c1eee6
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2015-07-01 10:27:16 +0100

    Copy Diego Perez's GA controller from PTSP for a base
    AI for testing purposes, changed package names to match new home.

---
+++ src/Controllers/ga/GAController.java
@@ -0,0 +1,16 @@
+package Controllers.ga;
+
+import Controllers.Controller;
+import FastGame.Action;
+import FastGame.CoopGame;
+
+/**
+ * Created by jwalto on 01/07/2015.
+ */
+public class GAController extends Controller {
+
+    @Override
+    public Action get(CoopGame game) {
+        return super.get(game);
+    }
+}
