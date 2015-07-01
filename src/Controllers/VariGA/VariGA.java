6ca7a1b23ece4e3038d2529b262794c38a494749
CrazyEssexGuy <pwillic@essex.ac.uk>
2015-07-01 13:20:05 +0100

    More work - hopefully obvious what the plan is here

--- src/Controllers/VariGA/VariGA.java
+++ src/Controllers/VariGA/VariGA.java
@@ -2,15 +2,33 @@ package Controllers.VariGA;
 
 import Controllers.Controller;
 import FastGame.Action;
+import FastGame.CoopGame;
 
 import java.util.Random;
 
 /**
+ *
+ * Do a 1 + 1 ES
  * Created by Piers on 01/07/2015.
  */
 public class VariGA extends Controller {
 
+    int iterations;
 
+    public VariGA(int iterations) {
+        this.iterations = iterations;
+    }
+
+    @Override
+    public Action get(CoopGame game) {
+        ActionSequence parent = new ActionSequence(3, 10, 1, 10);
+//        ActionSequence child = parent.mutate();
+        for(int i = 0; i < iterations; i++){
+            CoopGame workingGame = game.getClone();
+
+        }
+
+    }
 }
 
 class ActionSequence {
@@ -54,19 +72,53 @@ class ActionSequence {
      * @param lengthChance - Chance that the individual length of each parameter will be increased or decreased by 1 within the limits
      * @param actionChance - Chance that the individual action of each parameter will be randomly changed
      */
-    private void mutate(double numChance, double lengthChance, double actionChance) {
+    public void mutate(double numChance, double lengthChance, double actionChance) {
         double test = random.nextDouble();
         if (test > numChance) {
-            if(numberOfActions == maxLength){
-                // shrink
-            }else if (numberOfActions == minLength){
-                // grow
-            }else{
-                // grow or shrink
+            if (numberOfActions == maxLength) {
+                shrinkSequenceLength();
+            } else if (numberOfActions == minLength) {
+                growSequenceLength();
+            } else {
+                if (random.nextBoolean()) {
+                    shrinkSequenceLength();
+                } else {
+                    growSequenceLength();
+                }
+            }
+        }
+
+        for(int i = 0; i < numberOfActions; i++){
+            if(random.nextDouble() > lengthChance){
+                // mutate it
+                if(lengths[i] == maxLength) {
+                    lengths[i]--;
+                }else if(lengths[i] == minLength){
+                    lengths[i]++;
+                }else{
+                    lengths[i] += (random.nextBoolean())? 1 : -1;
+                }
+            }
+            if(random.nextDouble() > actionChance){
+                actions[i] = random.nextInt(Action.allActions.length);
             }
         }
     }
 
+    // increase size by 1
+    private void growSequenceLength() {
+        numberOfActions++;
+        int[] newLengths = new int[numberOfActions];
+        System.arraycopy(lengths, 0, newLengths, 0, lengths.length);
+        lengths = newLengths;
+    }
+
+    private void shrinkSequenceLength() {
+        numberOfActions--;
+        int[] newLengths = new int[numberOfActions];
+        System.arraycopy(lengths, 0, newLengths, 0, newLengths.length);
+        lengths = newLengths;
+    }
 
 
 }
