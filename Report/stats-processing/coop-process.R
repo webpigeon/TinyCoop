72129c0d4f90a056ab351fe665e200a5f97d2c41
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2015-07-04 15:39:12 +0100

    compairson graph data
    This doesn't work and I'm not sure why, occording to the web it should
    do...

--- Report/stats-processing/coop-process.R
+++ Report/stats-processing/coop-process.R
@@ -1,9 +1,10 @@
-setwd("D:\\graphs")
-library(ggplot2) 
+setwd("C:\\Users\\joseph\\Documents\\GitHub\\TinyCoop\\Report\\stats-processing")
+library(ggplot2)
+require(data.frame)
 
 # custom functions
 alpha<-0.05 # for a (1.00-alpha)=95% confidence interval
-c <- function() rm(list=ls())
+clear <- function() rm(list=ls())
 se <- function(x) sd(x) / sqrt(length(x))
 
 build_summary_score <- function (x) {
@@ -36,6 +37,18 @@ build_summary_ticks <- function (x) {
 	return(s)
 }
 
+# I'm not sure why this doesn't work...
+build_summary_map_score <- function (x) {
+	s <- x[, list(mean=mean(score),n=length(score),sd=sd(score),se=se(score)), by=c("agent1","maps")]
+	# Precalculate standard error of the mean (SEM)
+	s$sem <- s$sd/sqrt(s$n)
+	# Precalculate margin of error for confidence interval
+	s$me <- qt(1-alpha/2, df=s$n)*s$sem
+	return(s)
+}
+
+
+
 build_graph <- function (filename, title, s) {
 	summary(s)
 	png(filename, width = 900, height = 500, units="px")
@@ -64,6 +77,10 @@ games.scores <- build_summary_score(games.raw)
 games.ticks <- build_summary_ticks(games.raw)
 build_graph("scores-allmaps.png", "Scores across all maps for all agents", games.scores)
 build_graph("ticks-allmaps.png", "Ticks across all maps for all agents", games.ticks)
+
+# build RR graphs for all agents with the maps as a 2nd dimention
+#games.maps.scores <- build_summary_map_score(games.raw)
+#build_graph("scores-bymaps.png", "Scores by all maps for all agents", games.maps.scores)
 
 # All pairs avg score all maps
 #scores <- aggregate(games$score, list(agent1=games$agent1, agent2=games$agent2), mean)
