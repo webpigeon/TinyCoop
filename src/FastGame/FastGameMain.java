10f30424bbf53428cc672f94e2d1e94ebac3f7f4
CrazyEssexGuy <pwillic@essex.ac.uk>
2015-07-11 09:03:04 +0100

    But its borken a little

--- src/FastGame/FastGameMain.java
+++ src/FastGame/FastGameMain.java
@@ -27,15 +27,15 @@ public class FastGameMain {
 
 //        Controller c1 = new VariGA(true, 500);
         //Controller c1 = new RandomController();
-          Controller c1 = new NoOp();
+//          Controller c1 = new NoOp();
 
-//        Controller c1 = wasd;
+        Controller c1 = wasd;
 //        Controller c2 = new GAController(false);
 //        Controller c2 = new MCTS(false, 1000, 10, 45);
 //            Controller c2 = new VariGA(false, 2000);
 //        Controller c2 = new RandomController();
-//        Controller c2 = arrows;
-        Controller c2 = new NoOp();
+        Controller c2 = arrows;
+//        Controller c2 = new NoOp();
 
         CoopGame game = new CoopGame("data/maps/level5.txt");
 
